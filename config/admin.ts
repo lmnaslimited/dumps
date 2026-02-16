ff8a37b6b814b3d9f95bec9ba0cc07c0ca735a4d
Yuvaraj <yuvaraj.shanmugam@lmnas.com>
2026-02-16 14:30:48 +0530

    added preview feature enabled in config/admin.ts

--- config/admin.ts
+++ config/admin.ts
@@ -14,4 +14,34 @@ export default ({ env }) => ({
     nps: env.bool('FLAG_NPS', true),
     promoteEE: env.bool('FLAG_PROMOTE_EE', true),
   },
+   preview: {
+  enabled: true,
+  config: {
+    allowedOrigins: [env("CLIENT_URL")],
+    async handler(uid, { documentId, locale, status }) {
+      let slug = null;
+
+      try {
+        // Try fetching slug (for collection types)
+        const document = await strapi.documents(uid).findOne({
+          documentId,
+          fields: ["slug"],
+        });
+
+        slug = document?.slug ?? null;
+      } catch (err) {
+        // If slug doesn't exist (single type), ignore error
+        slug = null;
+      }
+
+      const urlSearchParams = new URLSearchParams({
+        secret: env("PREVIEW_SECRET"),
+        uid,
+        status,
+        ...(slug ? { slug } : {}), // Only attach slug if exists
+      });
+      return `${env("CLIENT_URL")}/api/preview?${urlSearchParams}`;
+    },
+  },
+},
 });
